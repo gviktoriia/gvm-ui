e655733d39b06a3430057506ff9062e93d2a38f3
gviktoriia <gviktoria10@ukr.net>
2024-05-07 22:38:16 +0300

    admin auth blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,7 @@ import StandardLayout from "../components/layouts/StandardLayout";
 import Blog from "../components/Blog/Blog";
 import { useSession } from "next-auth/react";
 import AdminBlog from "../components/Blog/AdminBlog";
+import { Box } from "@mui/material";
 
 const page = () => {
   const { data: session, status, update } = useSession();
@@ -13,7 +14,12 @@ const page = () => {
       title="Our Bloogs & Insights"
       subtitle="Welcome to the GVM-UI Blog, where we share valuable insights, industry updates, success stories, and more. Dive into our collection of articles crafted to inform, inspire, and engage our readers. "
       align="center"
-      content={status === "authenticated" ? <AdminBlog /> : <Blog />}
+      content={
+        <Box>
+          {status === "authenticated" && <AdminBlog />}
+          {!session && <Blog />}
+        </Box>
+      }
     />
   );
 };
