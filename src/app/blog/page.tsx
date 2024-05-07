ed97e515ef7515a28825a667c599e91d1061559b
gviktoriia <gviktoria10@ukr.net>
2024-05-07 22:44:34 +0300

    one more attempt

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,24 +2,14 @@
 import React from "react";
 import StandardLayout from "../components/layouts/StandardLayout";
 import Blog from "../components/Blog/Blog";
-import { useSession } from "next-auth/react";
-import AdminBlog from "../components/Blog/AdminBlog";
-import { Box } from "@mui/material";
 
 const page = () => {
-  const { data: session, status, update } = useSession();
-
   return (
     <StandardLayout
       title="Our Bloogs & Insights"
       subtitle="Welcome to the GVM-UI Blog, where we share valuable insights, industry updates, success stories, and more. Dive into our collection of articles crafted to inform, inspire, and engage our readers. "
       align="center"
-      content={
-        <Box>
-          {status === "authenticated" && <AdminBlog />}
-          {!session && <Blog />}
-        </Box>
-      }
+      content={<Blog />}
     />
   );
 };
--- src/app/components/Blog/Blog.tsx
+++ src/app/components/Blog/Blog.tsx
@@ -2,10 +2,24 @@ import { Box, Grid } from "@mui/material";
 import React from "react";
 import PostCard from "../cards/PostCard";
 import { POSTS } from "../../../../utils/postUtils";
+import { useSession } from "next-auth/react";
+import FilledButton from "../buttons/FilledButton";
 
 const Blog = () => {
+  const { data: session, status, update } = useSession();
+
   return (
     <Box>
+      {status === "authenticated" && (
+        <Box
+          sx={{
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <FilledButton title="New post" />
+        </Box>
+      )}
       <Grid container textAlign="center" justifyContent="center">
         {POSTS.map((post) => (
           <PostCard
