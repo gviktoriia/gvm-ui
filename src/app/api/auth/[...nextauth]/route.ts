c9db49c860e862881f9fa80106f80cd812ecb360
gviktoriia <gviktoria10@ukr.net>
2024-05-07 16:55:20 +0300

    auth file fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_SECRET_ID as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET as string,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "../../../../../utils/authOptions";
 
 const handler = NextAuth(authOptions);
 
--- src/app/components/Auth/Auth.tsx
+++ src/app/components/Auth/Auth.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import BorderedButton from "../buttons/BorderedButton";
+import { authOptions } from "../../../../utils/authOptions";
 
 export async function Auth() {
   const session = await getServerSession(authOptions);
@@ -9,10 +9,16 @@ export async function Auth() {
   return (
     <Box>
       {session && (
-        <div>
-          <p>Signed in as {session.user && session.user.name}</p>
-          <a href="/api/auth/signout">Sign out</a>
-        </div>
+        <Box display="flex" flexDirection="row" gap={2}>
+          <Typography
+            sx={{
+              color: "black",
+            }}
+          >
+            Signed in as {session.user && session.user.name}
+          </Typography>
+          <BorderedButton title="Logout" link="/api/auth/signout" />
+        </Box>
       )}
 
       {!session && <BorderedButton title="Login" link="/login" />}
